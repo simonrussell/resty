39efe22fc2380d16c22f66576ac31295d6ac91dc
Simon Russell <simon@bellyphant.com>
2011-06-06 21:41:07 +1000

    More stuff on Resty objects.

--- lib/resty.rb
+++ lib/resty.rb
@@ -1,5 +1,21 @@
 class Resty
   
+  def initialize(attributes)
+    @attributes = attributes
+  end
+
+  def respond_to_missing?(name, include_private)
+    @attributes.key?(name.to_s)
+  end
+
+  def method_missing(name)
+    if @attributes.key?(name.to_s)
+      @attributes[name.to_s]
+    else
+      super
+    end
+  end
+  
 end
 
 require File.join(File.dirname(__FILE__), 'resty', 'attributes')
--- spec/lib/resty_spec.rb
+++ spec/lib/resty_spec.rb
@@ -2,8 +2,27 @@ require 'spec_helper'
 
 describe Resty do
 
-  it "should work" do
-    pending
+  describe "attribute methods" do
+
+    let(:attributes) { { 'name' => 'bob' } }
+    subject { Resty.new(attributes) }
+
+    it "should respond to known attribute" do
+      subject.should respond_to(:name)
+    end
+
+    it "should not respond to unknown attribute" do
+      subject.should_not respond_to(:biscuits)
+    end
+
+    it "should return known attribute when method called" do
+      subject.name.should == 'bob'
+    end
+
+    it "should raise error for unknown attribute when method called" do
+      lambda { subject.biscuits }.should raise_error(NoMethodError)
+    end
+
   end
 
 end
