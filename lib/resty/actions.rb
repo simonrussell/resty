17f14ef48288adf94e6cfb2cf2f1186dd323fb0b
Simon Russell <simon@playup.com>
2011-06-07 11:37:59 +1000

    Params on actions.

--- lib/resty/actions.rb
+++ lib/resty/actions.rb
@@ -4,12 +4,20 @@ class Resty::Actions
     @definitions = definitions || {}
   end
   
-  def perform!(name)
+  def perform!(name, parameters = nil)
     raise "unknown action #{name}" unless exist?(name)
     href = @definitions[name].fetch(':href') { raise "no href for action #{name}" }
     method = @definitions[name].fetch(':method') { raise "no method for action #{name}" }
     
-    Resty::Transport.request_json(href, method)
+    if parameters
+      body = parameters.to_json
+      mimetype = 'application/json'
+    else
+      body = nil
+      mimetype = nil
+    end
+    
+    Resty::Transport.request_json(href, method, body, mimetype)
   end
 
   def exist?(name)
--- spec/lib/resty/actions_spec.rb
+++ spec/lib/resty/actions_spec.rb
@@ -7,12 +7,17 @@ describe Resty::Actions do
     subject { Resty::Actions.new('bake' => { ':href' => 'http://blah.blah/123/bake', ':method' => 'POST' }) }
   
     it "should call the action when performed!" do
-      Resty::Transport.should_receive(:request_json).with('http://blah.blah/123/bake', 'POST')
+      Resty::Transport.should_receive(:request_json).with('http://blah.blah/123/bake', 'POST', nil, nil)
       subject.perform!('bake')
     end
   
     it "should raise error on unknown action" do
       lambda { subject.perform!('fry') }.should raise_error
+    end
+    
+    it "should pass parameters" do
+      Resty::Transport.should_receive(:request_json).with('http://blah.blah/123/bake', 'POST', { blah: 'bling' }.to_json, 'application/json')
+      subject.perform!('bake', blah: 'bling')
     end
 
   end
