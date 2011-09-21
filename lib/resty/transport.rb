58b6ecf6e5f351750b19796739f271f2f7f397e2
Dave Lyons <dalyons@gmail.com>
2011-09-21 14:45:58 +1000

    Proposed implementation of default url params

--- lib/resty/transport.rb
+++ lib/resty/transport.rb
@@ -1,12 +1,16 @@
 class Resty::Transport
   
+  class << self
+    attr_accessor :default_href_params
+  end
+
   def self.request_json(href, method = 'GET', body = nil, content_type = nil)
   
     headers = content_type ? { :content_type => content_type } : nil
     payload = body || ''
-  
+
     response = RestClient::Request.execute(
-      :url => href,
+      :url => add_default_params(href),
       :method => method,
       :payload => payload,
       :headers => headers
@@ -14,11 +18,23 @@ class Resty::Transport
     
     if !(response.nil? || response =~ /\A\s*\Z/)
       JSON.parse(response)
-    elsif response.headers[:location]
+    elsif !response.nil? && response.headers[:location]
       { ':href' => response.headers[:location] }
     else
       nil
     end
+  end
+
+  private
+  
+  PARAM_REGEX = /\?/
+  
+  def self.add_default_params(href)
+    return href if !default_href_params || default_href_params.empty?
+    
+    separator = (href =~ PARAM_REGEX ? '&' : '?')
+    params = default_href_params.map { |k, v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')
+    "#{href}#{separator}#{params}"
   end
 
 end
